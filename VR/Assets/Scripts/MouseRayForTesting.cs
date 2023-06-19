e65ac5aaa4fe2f9820acfddcc1b56c2b34adb97b
JasonAnderson0 <s201293@students.aie.edu.au>
2023-06-19 15:22:19 +0930

    VR shooting works

--- VR/Assets/Scripts/MouseRayForTesting.cs
+++ VR/Assets/Scripts/MouseRayForTesting.cs
@@ -2,40 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using TMPro;
 
 public class MouseRayForTesting : MonoBehaviour
 {
-    private PlayerInput playerInput;
+    public InputActionManager playerInput;
     public GameObject setTarget;
     private TargetManager targetManager;
-    // Start is called before the first frame update
+    public GameObject gun;
+    public LayerMask LM;
+    public TextMeshPro text;
+    int shots = 0;
+    bool castHit;
+    
+
+
     void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
+        playerInput = GetComponent<InputActionManager>();
         targetManager = FindObjectOfType<TargetManager>();
-        playerInput.actions["Click"].performed += TestFire;
+        //playerInput.actionAssets["Select"].performed += TestFire;
+        playerInput.actionAssets[0].FindAction("Test").performed += Fire;
+        //testAction.performed += DebugFire;
+        //playerInput.
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1000))
-        {
-            if (hit.collider.gameObject.tag == "TargetCollider")
-            {
-                setTarget = hit.collider.transform.parent.gameObject;
-            }
-            else
-            {
-                setTarget = null;
-            }
-        }
-        else
-        {
-            setTarget = null;
-        }
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit, 1000))
+        //{
+        //    if (hit.collider.gameObject.tag == "TargetCollider")
+        //    {
+        //        setTarget = hit.collider.transform.parent.gameObject;
+        //    }
+        //    else
+        //    {
+        //        setTarget = null;
+        //    }
+        //}
+        //else
+        //{
+        //    setTarget = null;
+        //}
+
+
+
     }
     private void TestFire(InputAction.CallbackContext context)
     {
@@ -43,5 +58,29 @@ public class MouseRayForTesting : MonoBehaviour
         {
             targetManager.DropTarget(setTarget);
         }
+    }
+
+    void Fire(InputAction.CallbackContext context)
+    {
+        if (!castHit)
+            return;
+
+        targetManager.DropTarget(setTarget);
+
+
+        shots++;
+
+        text.text = ("You hit button no hit " + shots);
+
+    }
+
+
+    public void Active()
+    {
+        castHit = true;
+    }
+    public void Inactive()
+    {
+        castHit = false;
     }
 }
