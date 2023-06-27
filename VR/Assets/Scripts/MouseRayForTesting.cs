851d9dba9ac3cd65233fc2aac7662b9776fc86a6
JasonAnderson0 <s201293@students.aie.edu.au>
2023-06-27 11:18:29 +0930

    headset, sounds and haptic working. TODO: rework haptic for proper use

--- VR/Assets/Scripts/MouseRayForTesting.cs
+++ VR/Assets/Scripts/MouseRayForTesting.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MouseRayForTesting : MonoBehaviour
 {
     public InputActionManager playerInput;
     public Gun Gun;
 
+    //test haptic
+    public HapticInteractable haphap;
+    public XRBaseController controller;
+
     void Start()
     {
         playerInput = GetComponent<InputActionManager>();
-        playerInput.actionAssets[0].FindAction("Fire").performed += Fire;
-        //playerInput.actionAssets[0].FindAction("Fire").performed += Fire;
+        playerInput.actionAssets[0].FindAction("FireL").performed += FireL;
+        playerInput.actionAssets[0].FindAction("FireR").performed += FireR;
         playerInput.actionAssets[0].FindAction("Drop").performed += DropMagazine;
         playerInput.actionAssets[0].FindAction("SwapL").performed += Swap;
         playerInput.actionAssets[0].FindAction("SwapR").performed += Swap;
+
+        //haphap = FindObjectOfType<HapticInteractable>();
     }
-    void Fire(InputAction.CallbackContext context)
+    void FireL(InputAction.CallbackContext context)
     {
-        Gun.Fire();
-    }    
+        //haphap.TriggerHaptic(controller);
+
+
+        if (Gun.curParent == Gun.attachPointL)
+        {
+            Gun.Fire();
+
+        }
+
+    }
+
+    void FireR(InputAction.CallbackContext context)
+    {
+        haphap.TriggerHaptic(controller);
+
+
+
+        if (Gun.curParent == Gun.attachPointR)
+            Gun.Fire();
+    }
 
     void DropMagazine(InputAction.CallbackContext context)
     {
